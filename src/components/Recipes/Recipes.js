00ea711d5a58463f9baec2187292fdfed0b799b5
Luke Klymshyn <unseenmountain@gmail.com>
2020-05-07 18:50:57 -0700

    added new stuff

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -3,7 +3,7 @@ import React from "react"
 function Recipes () {
     return(
         <div>
-            <h1>Recipes</h1>
+            <h1><iframe width="560" height="315" src="https://www.youtube.com/embed/EMZkTLy7tvc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></h1>
         </div>
     )
 }
